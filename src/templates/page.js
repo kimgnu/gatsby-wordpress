d1cdf09d974472a206553844feba5715213fa798
kimgnu <kimgnu@yahoo.com>
2020-01-06 18:30:31 +0900

    add menus through new wp-api-menus api

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,10 +1,9 @@
 import React from 'react'
+import Layout from '../components/layout'
 
 export default ({pageContext}) => (
-    <div>
-        <h1>
-            {pageContext.title}
-        </h1>
+    <Layout>
+        <h1 dangerouslySetInnerHTML={{__html: pageContext.title}} />
         <div dangerouslySetInnerHTML={{__html: pageContext.content}} />
-    </div>
+    </Layout>
 )
