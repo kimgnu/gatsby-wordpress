e4ccc199de4c437968ba5f1333209faad327cd1e
kimgnu <kimgnu@yahoo.com>
2020-01-07 11:13:11 +0900

    add SiteInfo to get site title and description

--- src/components/main-menu.js
+++ src/components/main-menu.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import {graphql, StaticQuery, Link} from 'gatsby'
 import styled from 'styled-components'
+import SiteInfo from './site-info'
 
 const MainMenuWrapper = styled.div`
     display: flex;
     background-color: rgb(3, 27, 77);
+`
+
+const MainMenuInner = styled.div`
+    max-width: 960px;
+    margin: 0 auto;
+    display: flex;
+    width: 960px;
+    height: 100%;
 `
 
 const MenuItem = styled(Link)`
@@ -33,11 +42,14 @@ const MainMenu = () => (
     }
     `} render={props => (
         <MainMenuWrapper>
-            {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => (
-                <MenuItem to={item.object_slug} key={item.title}>
-                    {item.title}
-                </MenuItem>
-            ))}
+            <MainMenuInner>
+                <SiteInfo />
+                {props.allWordpressWpApiMenusMenusItems.edges[0].node.items.map(item => (
+                    <MenuItem to={item.object_slug} key={item.title}>
+                        {item.title}
+                    </MenuItem>
+                ))}
+            </MainMenuInner>
         </MainMenuWrapper>
     )} />
 )
