d4e5a31382c5de757750b40387e323446b62708d
kimgnu <kimgnu@yahoo.com>
2020-01-08 11:42:22 +0900

    fix open sans font

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,7 +10,7 @@ import styled, {createGlobalStyle} from 'styled-components'
 import MainMenu from "./main-menu"
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css?family=Open+Sans');
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i,800,800i&display=swap');
   body {
     font-family: 'Open Sans', sans-serif;
     margin: 0 !important;
